8aea951d2652429ccf4813870d79715cd7b3694d
Leonardo Omaña <leotangram@gmail.com>
2019-06-15 16:40:17 -0500

    Use effect, consult API.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [cite, setCite] = useState({})
+
+  const consultAPI = async () => {
+    const result = await axios(
+      'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    )
+
+    // Agregar el resultado del API al state; (Similar a this.setState)
+    setCite(result.data[0])
+  }
+
+  // Consulta a una rest api
+  useEffect(() => {
+    consultAPI()
+  }, [])
+
+  console.log(cite)
+
   return <p>Hola</p>
 }
 
